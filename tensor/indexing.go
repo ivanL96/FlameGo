196ff1eb9c4d37fc4eb8ca32ac466d208601d0d3
Ivan <ivanlukanskiy@gmail.com>
2023-05-20 17:27:20 +0400

    negative index handling

--- tensor/indexing.go
+++ tensor/indexing.go
@@ -14,8 +14,8 @@ func (tensor *Tensor[T]) Get(indices ...int) T {
 	return tensor.data[flatIndex]
 }
 
-// returns sub data for given indices. Doesn't copy tensor
-func (tensor *Tensor[T]) View(indices ...int) *Tensor[T] {
+// returns sub data for given indices.
+func (tensor *Tensor[T]) Index(indices ...int) *Tensor[T] {
 	if len(indices) == 0 {
 		panic("At leat one index is required in View")
 	}
@@ -32,12 +32,20 @@ func (tensor *Tensor[T]) View(indices ...int) *Tensor[T] {
 	}
 	flatIndex := 0 // index of the first elem in the sub tensor
 	for i, ind := range indices {
+		// resolve negative indexes
+		if ind < 0 {
+			ind = int(tensor.shape[i]) + ind
+		}
+		if ind < 0 {
+			panic(fmt.Sprintf("Index %v is out of bounds", ind))
+		}
 		flatIndex += tensor.strides[i] * ind
 	}
 	// fmt.Println("flatIndex", flatIndex, tensor.data[flatIndex])
 	if len(indices) == len(tensor.shape) {
 		return InitTensor([]T{tensor.data[flatIndex]}, Shape{1})
 	} else {
+		// TODO add case for contiguous data: dim_order 0,1,2,3...
 
 		innerShape := tensor.shape[len(indices):]
 		// fmt.Println("innerShape", innerShape)
@@ -51,19 +59,11 @@ func (tensor *Tensor[T]) View(indices ...int) *Tensor[T] {
 		row := int(innerShape[len(innerShape)-1]) // deepest axis
 		rowStride := tensor.strides[len(tensor.strides)-1]
 		for j := 0; j < int(innerShapeProd); j++ {
-			rem := (j / row) * 2
+			rem := (j / row) * row
 			count := j % row
 			deepIndex := (flatIndex + rem) + rowStride*count
 			subData[j] = tensor.data[deepIndex]
 		}
 		return InitTensor(subData, innerShape)
 	}
-}
-
-// same as tensor.View but with copying the data
-func (tensor *Tensor[T]) Index(indices ...int) *Tensor[T] {
-	sub_tensor := tensor.View(indices...)
-	sub_data_copy := make([]T, len(sub_tensor.data))
-	copy(sub_data_copy, sub_tensor.data)
-	return InitTensor(sub_data_copy, sub_tensor.shape)
 }
