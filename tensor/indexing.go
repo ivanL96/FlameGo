b1854a4c638a2d04d5097706184184aaba6d2594
Ivan <ivanlukanskiy@gmail.com>
2023-05-22 03:27:40 +0400

    fixed indexing

--- tensor/indexing.go
+++ tensor/indexing.go
@@ -30,9 +30,6 @@ func (tensor *Tensor[T]) Index(indices ...int) *Tensor[T] {
 		panic("Too many indices")
 	}
 
-	newShape := make(Shape, n_dims-n_indices)
-	copy(newShape, tensor.shape[n_indices:])
-
 	flatIndex := 0 // index of the first elem in the sub tensor
 	for i, ind := range indices {
 		// resolve negative indexes
@@ -56,18 +53,29 @@ func (tensor *Tensor[T]) Index(indices ...int) *Tensor[T] {
 		return InitTensor(subData, innerShape)
 	}
 
+	innerStrides := tensor.strides[n_indices:]
+
 	// not continuous data. i.e. transposed tensor
 	var innerShapeProd Dim = 1
 	for _, dim := range innerShape {
 		innerShapeProd *= dim
 	}
 
+	subShape := innerShape
+	// expand innerShape
+	// TODO this is extra step, better to do something with the loop
+	if len(innerShape) == 1 {
+		innerShape = Shape{1, innerShape[0]}
+		innerStrides = []int{innerStrides[0], innerStrides[0]}
+	}
+
 	// prealloc output
 	subData := make([]T, innerShapeProd)
-	innerStrides := tensor.strides[n_indices:]
 	innermostStride := tensor.strides[len(tensor.strides)-1]
 	row := int(innerShape[len(innerShape)-1]) // innermost axis
-	for i := len(innerStrides) - 2; i >= 0; i-- {
+	//number of dims around the 'row. Cannot be zero'
+	numDims := len(innerStrides) - 2
+	for i := numDims; i >= 0; i-- {
 		stride := innerStrides[i]
 		subDataIdx := 0
 		for s := 0; s < int(innerShape[i]); s++ {
@@ -79,5 +87,5 @@ func (tensor *Tensor[T]) Index(indices ...int) *Tensor[T] {
 			}
 		}
 	}
-	return InitTensor(subData, innerShape)
+	return InitTensor(subData, subShape)
 }
