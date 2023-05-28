d63d083c2536329d3244b6ef27e18daee832a1cc
Ivan <ivanlukanskiy@gmail.com>
2023-05-29 02:14:14 +0400

    added sdbmHash

--- tensor/hash.go
+++ tensor/hash.go
@@ -2,12 +2,24 @@ package tensor
 
 import "gograd/tensor/types"
 
+// https://golangprojectstructure.com/hash-functions-go-code/
+
 func djb2[T types.TensorType](data []T) uint64 {
 	hash := uint64(5381)
 
 	for _, b := range data {
 		hash += uint64(b) + hash + hash<<5
 	}
+	return hash
+}
+
+func sdbmHash[T types.TensorType](data []T) uint64 {
+	var hash uint64
+
+	for _, b := range data {
+		hash = uint64(b) + (hash << 6) + (hash << 16) - hash
+	}
+
 	return hash
 }
 
