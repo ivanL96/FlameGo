9d7dd2f336f646d95272651e9f8724bfd7622864
Ivan <ivanlukanskiy@gmail.com>
2023-05-30 20:40:59 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np	
-a = np.ones((3, 2))*2
-b = np.ones((3, 1))*3
-# print(a, b)
-c = np.reshape(np.arange(12), (3,2,2)) # [0 1 {2 3} 4 5]
-# [0,1,2,3,4,5,6,7,8,9,10,11,12]
-print(c, c[1]) # 2,3
+import time
+size = 5
+a = np.reshape(np.arange(size*size),(size, size))
+b = np.reshape(np.arange(size*size),(size, size))
+c = a @ b
+print(c)
