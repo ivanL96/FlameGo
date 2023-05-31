df7d66d5ab087870a1af16c8b855e5bacd5078b1
Ivan <ivanlukanskiy@gmail.com>
2023-05-31 20:58:19 +0400

    ToString() shrink tensor height

--- tensor/tensor_repr.go
+++ tensor/tensor_repr.go
@@ -8,8 +8,14 @@ import (
 	"strings"
 )
 
+const _LINES_LIMIT int = 100
+const _HEAD_SIZE int = 10
+
 type printSettings struct {
 	whitespaceOffset int
+	linesAdded       int
+	linesSkipped     int
+	total_lines      int
 	lastCharClosed   bool
 }
 
@@ -36,18 +42,46 @@ func joinData[T types.TensorType](sb *strings.Builder, data []T) {
 	sb.WriteString(strings.Join(stringData, ", "))
 }
 
-func stringRepr[T types.TensorType](sb *strings.Builder, tensor *Tensor[T], ps *printSettings) {
-	// TODO shrink height of printed array
-	if ps.lastCharClosed {
+func printIndent(sb *strings.Builder, ps *printSettings) {
+	if ps.lastCharClosed && ps.linesSkipped == 0 {
 		for j := 0; j < ps.whitespaceOffset; j++ {
 			sb.WriteRune(' ')
 		}
 		ps.lastCharClosed = false
 	}
-	sb.WriteRune('[')
+}
+
+func stringRepr[T types.TensorType](sb *strings.Builder, tensor *Tensor[T], ps *printSettings) {
+	printIndent(sb, ps)
+
+	// write line
 	if len(tensor.shape) == 1 { // at this point tensor is subset created from mainTensor.Index()
+		if ps.total_lines >= _LINES_LIMIT {
+			// if tensor is too big then should shrink lines after some limit (_HEAD_SIZE)
+			if ps.linesAdded >= _HEAD_SIZE {
+				// shrink height
+				if ps.linesSkipped == 0 {
+					sb.WriteString("...\n")
+					ps.linesSkipped++
+					return
+				} else if ps.linesSkipped < ps.total_lines-_HEAD_SIZE*2 {
+					ps.linesSkipped++
+					return
+				} else {
+					// stop skipping lines, reset counters
+					ps.linesAdded = 0
+					ps.linesSkipped = 0
+					ps.lastCharClosed = true
+					printIndent(sb, ps)
+				}
+			}
+			ps.linesAdded++
+		}
+
+		sb.WriteRune('[')
 		lastDim := tensor.shape[0]
 		if lastDim > 30 {
+			// shrink row
 			joinData(sb, tensor.data[:5])
 			sb.WriteString("...")
 			joinData(sb, tensor.data[len(tensor.data)-5:])
@@ -60,14 +94,20 @@ func stringRepr[T types.TensorType](sb *strings.Builder, tensor *Tensor[T], ps *
 		return
 	}
 
+	// write sub tensor
+	if ps.linesSkipped == 0 {
+		sb.WriteRune('[')
+	}
 	nrows := int(tensor.shape[0])
 	for i := 0; i < nrows; i++ {
-		if i > 0 {
-			sb.WriteRune('\n')
+		if ps.linesSkipped == 0 {
+			if i > 0 {
+				sb.WriteRune('\n')
+			}
+			ps.whitespaceOffset += 1
 		}
-		ps.whitespaceOffset += 1
 		stringRepr(sb, tensor.Index(i), ps)
-		if i == nrows-1 {
+		if i == nrows-1 && ps.linesSkipped == 0 {
 			sb.WriteRune(']')
 			ps.whitespaceOffset -= 1
 			ps.lastCharClosed = true
@@ -80,10 +120,11 @@ func (tensor *Tensor[T]) ToString() string {
 	for _, dim := range tensor.shape {
 		shape_prod *= dim
 	}
-	var (
-		sb strings.Builder
-		ps printSettings
-	)
+	total_lines := int(shape_prod / tensor.shape[len(tensor.shape)-1])
+	var sb strings.Builder
+	ps := printSettings{
+		total_lines: total_lines,
+	}
 	stringRepr(&sb, tensor, &ps)
 	strData := sb.String()
 
