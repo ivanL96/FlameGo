b04ae4497363f5465a5c38809d4a5d27b7a5be57
Ivan <ivanlukanskiy@gmail.com>
2023-06-03 22:34:26 +0400

    Update ops_test.go

--- benchmarks/ops_test.go
+++ benchmarks/ops_test.go
@@ -36,11 +36,24 @@ func BenchmarkBigAdd(b *testing.B) {
 	fmt.Println(out.Get(999, 999))
 }
 
+// scalar op
+// BenchmarkBigMul-8             32          34.973.966 ns/op          626015 B/op          2 allocs/op
+// BenchmarkBigMul-8             31          40.353.242 ns/op          646199 B/op          2 allocs/op
+// BenchmarkBigMul-8             39          37.115.072 ns/op          513648 B/op          2 allocs/op
+// mul noasm
+// BenchmarkBigMul-8            434           2.422.563 ns/op           46155 B/op          0 allocs/op
+// BenchmarkBigMul-8            469           2.410.727 ns/op           42711 B/op          0 allocs/op
+// BenchmarkBigMul-8            480           2.434.254 ns/op           41732 B/op          0 allocs/op
+// avx2
+// BenchmarkBigMul-8           1047           1.095.734 ns/op           19132 B/op          0 allocs/op
+// BenchmarkBigMul-8           1172           1.150.072 ns/op           17090 B/op          0 allocs/op
+// BenchmarkBigMul-8           1178           1.024.620 ns/op           17003 B/op          0 allocs/op
+// shape 1000x1000
 func BenchmarkBigMul(b *testing.B) {
-	scalar := tensor.Scalar[int32](1000)
-	a1 := tensor.Range[int32](1000000).Reshape(1000, 1000).Div(scalar, nil)
-	a2 := tensor.Range[int32](1000000).Reshape(1000, 1000).Div(scalar, nil)
-	out := tensor.CreateEmptyTensor[int32](1000, 1000)
+	scalar := tensor.Scalar[float32](1000)
+	a1 := tensor.Range[float32](1000000).Reshape(1000, 1000).Div(scalar, nil)
+	a2 := tensor.Range[float32](1000000).Reshape(1000, 1000).Div(scalar, nil)
+	out := tensor.CreateEmptyTensor[float32](1000, 1000)
 	for i := 0; i < b.N; i++ {
 		a1.Mul(a2, out)
 	}
