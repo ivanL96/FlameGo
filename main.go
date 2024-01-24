ba34a685bd75262d47d776c53b97871b9c08e603
Ivan <ivanlukanskiy@gmail.com>
2024-01-24 17:06:19 +0100

    Update main.go

--- main.go
+++ main.go
@@ -19,13 +19,24 @@ func main() {
 	// #  [152, 174, 196, 218]
 	// #  [248, 286, 324, 362]
 	// #  [344, 398, 452, 506]],
-	var dim types.Dim = 10
-	ten := tensor.Scalar[float32](10)
-	aa := tensor.Range[float32](int(dim*dim)).Reshape(dim, dim).Div(ten, nil)
-	bb := tensor.Range[float32](int(dim*dim)).Reshape(dim, dim).Div(ten, nil)
-	fmt.Println(aa.ToString())
-	fmt.Println(bb.ToString())
-	cc := aa.MatMul(bb)
+
+	// var dim types.Dim = 10
+	// ten := tensor.Scalar[float32](10)
+	// aa := tensor.Range[float32](int(dim*dim)).Reshape(dim, dim).Div(ten)
+	// bb := tensor.Range[float32](int(dim*dim)).Reshape(dim, dim).Div(ten)
+	// fmt.Println(aa.ToString())
+	// fmt.Println(bb.ToString())
+	// cc := aa.MatMul(bb)
 	// res := tensor.AsType[float32, int64](cc)
-	fmt.Println(cc.ToString())
+	// fmt.Println(cc.ToString())
+	x := grad.Variable[float32](tensor.Ones[float32](3, 3).Fill(3))
+	x.Requires_grad = false
+	w := grad.Variable[float32](tensor.Ones[float32](3, 3).Fill(2))
+	b := grad.Variable[float32](tensor.Ones[float32](3, 3).Fill(5))
+	z := w.Mul(x) // 2*3+5
+	z.Backward()
+	fmt.Println("z.grad", z.Grad.ToString())
+	fmt.Println("w.grad", w.Grad.ToString())
+	fmt.Println("x.grad", x.Grad.ToString())
+	fmt.Println("b.grad", b.Grad.ToString())
 }
