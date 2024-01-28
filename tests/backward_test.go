918a3ff5fad6adb8cb146f14cd683f649fe51ec0
Ivan <ivanlukanskiy@gmail.com>
2024-01-28 02:40:14 +0100

    Update backward_test.go

--- tests/backward_test.go
+++ tests/backward_test.go
@@ -69,3 +69,46 @@ func TestGradMul(t *testing.T) {
 	deriv = grad.NumericDeriv(grad.EPSILON, 5, _mul2)
 	assert(t, math.Abs(4-deriv) <= 0.0001)
 }
+
+func TestGradMatMul(t *testing.T) {
+	a := grad.Variable[float32](tensor.Range[float32](5).Reshape(1, 5))
+	b := grad.Variable[float32](tensor.Range[float32](5).Reshape(5, 1))
+	z := a.MatMul(b)
+	z.Backward(nil)
+	assertEqualSlices(t, z.Value.Shape(), types.Shape{1, 1})
+	assertEqualSlices(t, z.Value.Data(), []float32{30})
+	assertEqualSlices(t, a.Grad.Data(), []float32{0, 1, 2, 3, 4})
+	assertEqualSlices(t, a.Grad.Shape(), types.Shape{1, 5})
+	assertEqualSlices(t, b.Grad.Data(), []float32{0, 1, 2, 3, 4})
+	assertEqualSlices(t, b.Grad.Shape(), types.Shape{5, 1})
+}
+
+func TestGradMatMulMean(t *testing.T) {
+	a := grad.Variable[float32](tensor.Range[float32](8).Reshape(4, 2))
+	b := grad.Variable[float32](tensor.Range[float32](10).Reshape(2, 5))
+	z := a.MatMul(b).Mean()
+	z.Backward(nil)
+	assertEqualSlices(t, z.Value.Shape(), types.Shape{1})
+	assertEqualSlices(t, z.Value.Data(), []float32{34})
+	assertEqualSlices(t, a.Grad.Data(), []float32{
+		0.5, 1.75, 0.50, 1.75, 0.5, 1.75, 0.5, 1.75})
+	assertEqualSlices(t, a.Grad.Shape(), types.Shape{4, 2})
+	assert(t, b.Grad.IsAllClose(
+		tensor.CreateTensor[float32]([]float32{0.6, 0.6, 0.6, 0.6, 0.6, 0.8, 0.8, 0.8, 0.8, 0.8}, types.Shape{2, 5}),
+		0.00001,
+	))
+	assertEqualSlices(t, b.Grad.Shape(), types.Shape{2, 5})
+}
+
+func TestGradMatMulMSE(t *testing.T) {
+	x := grad.Constant[float32](tensor.Range[float32](10).Div(tensor.Scalar[float32](10)).Reshape(10, 1))
+	y := grad.Constant[float32](tensor.Range[float32](10).Reshape(10, 1))
+	w := grad.Variable[float32](tensor.Range[float32](1).Reshape(1, 1))
+	b := grad.Variable[float32](tensor.Range[float32](1).Reshape(1, 1))
+	yhat := x.MatMul(w).Add(b)
+	loss := yhat.MSE(y)
+	loss.Backward(nil)
+	assertEqualSlices(t, loss.Value.Data(), []float32{28.5})
+	assertEqualSlices(t, w.Grad.Data(), []float32{-5.7})
+	assertEqualSlices(t, b.Grad.Data(), []float32{-9})
+}
