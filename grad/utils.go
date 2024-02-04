74fd5313e7b87a70b6e0876e56a5ff2befba7b3a
Ivan <ivanlukanskiy@gmail.com>
2024-02-04 21:39:40 +0100

    ToOneHot encoding utils

---
+++ grad/utils.go
@@ -0,0 +1,39 @@
+package grad
+
+import (
+	"fmt"
+	"gograd/tensor/types"
+	"math"
+)
+
+func (y *variable[T]) ToOneHot(classes uint) *variable[T] {
+	ndims := len(y.Value.Shape().Squeeze())
+	if ndims > 1 {
+		panic(fmt.Sprintf("variable y must have a vector-like shape, got %v", y.Value.Shape()))
+	}
+	size := y.Value.Shape()[0]
+	oneHotData := make([]T, int(size)*int(classes))
+	for i := 0; i < int(size); i++ {
+		classidx := int(y.Value.Data()[i])
+		if classidx >= int(classes) {
+			panic(fmt.Sprintf("too few classes. tensor has element '%v' which must be less than 'classes' %v", classidx, classes))
+		}
+		oneHotData[i*int(classes)+classidx] = 1
+	}
+	return VarFrom[T](oneHotData, types.Shape{size, types.Dim(classes)})
+}
+
+// number of classes is auto detected
+func (y *variable[T]) ToOneHotAuto() *variable[T] {
+	_max := uint(y.Value.Max(false).Data()[0] + 1)
+	return y.ToOneHot(_max)
+}
+
+func hasNaN[T types.TensorType](data []T) bool {
+	for i := 0; i < len(data); i++ {
+		if math.IsNaN(float64(data[i])) {
+			return true
+		}
+	}
+	return false
+}
