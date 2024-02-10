5715bfdf2da1b19220f6bb827b53c8cf29a75acd
Ivan <ivanlukanskiy@gmail.com>
2024-02-10 18:57:12 +0100

    removed unused src

--- tensor/internal/intrinsics/amd64/stubs.go
+++ tensor/internal/intrinsics/amd64/stubs.go
@@ -10,14 +10,3 @@ func _mm256_dot(a, b, n, ret unsafe.Pointer)
 
 //go:noescape
 func _mm512_dot(a, b, n, ret unsafe.Pointer)
-
-//go:noescape
-func _mm256_mul_to(a, b, c, n unsafe.Pointer)
-
-//go:noescape
-func _mm512_mul_to(a, b, c, n unsafe.Pointer)
-
-//go:noescape
-func _mm256_mul_const_to(a, b, c, n unsafe.Pointer)
-
-// func _mm256_add_to(a, b, c, n unsafe.Pointer)
