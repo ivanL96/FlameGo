4a9c1021bc191fe8a754c95dfe1e43efa066e05e
Ivan <ivanlukanskiy@gmail.com>
2024-02-10 18:55:32 +0100

    Update avx2_int.c

--- tensor/internal/intrinsics/src/avx2_int.c
+++ tensor/internal/intrinsics/src/avx2_int.c
@@ -17,9 +17,10 @@ void _mm256i_add_to(int32_t *a, int32_t *b, int32_t *c, int64_t n)
         _mm256_storeu_si256((__m256i *)&c[i * 8], v);
     }
 
+    int offset = epoch * 8;
     for (int i = 0; i < remain; i++)
     {
-        c[epoch * 8 + i] = a[epoch * 8 + i] + b[epoch * 8 + i];
+        c[offset + i] = a[offset + i] + b[offset + i];
     }
 }
 
@@ -37,9 +38,10 @@ void _mm256i_add_to_const(int32_t *a, int32_t b, int32_t *c, int64_t n)
         _mm256_storeu_si256((__m256i *)&c[i * 8], v);
     }
 
+    int offset = epoch * 8;
     for (int i = 0; i < remain; i++)
     {
-        c[epoch * 8 + i] = a[epoch * 8 + i] + b;
+        c[offset + i] = a[offset + i] + b;
     }
 }
 
@@ -58,9 +60,10 @@ void _mm256i_mul_to(int32_t *a, int32_t *b, int32_t *c, int64_t n)
         _mm256_storeu_si256((__m256i *)&c[i * 8], v);
     }
 
-    for (int i = epoch * 8; i < n; i++)
+    int offset = epoch * 8;
+    for (int i = 0; i < remain; i++)
     {
-        c[epoch * 8 + i] = a[epoch * 8 + i] * b[epoch * 8 + i];
+        c[offset + i] = a[offset + i] * b[offset + i];
     }
 }
 
@@ -73,16 +76,15 @@ void _mm256i_mul_to_const(int32_t *a, int32_t b, int32_t *c, int64_t n)
     #pragma omp parallel for
     for (int i = 0; i < epoch; i++)
     {
-        __m256i v1 = _mm256_loadu_si256((__m256i*)a);
+        __m256i v1 = _mm256_loadu_si256((__m256i*)&a[i * 8]);
         __m256i v = _mm256_mul_epi32(v1, v2);
-        _mm256_storeu_si256((__m256i*)c, v);
-        a += 8;
-        c += 8;
+        _mm256_storeu_si256((__m256i*)&c[i * 8], v);
     }
 
+    int offset = epoch * 8;
     for (int i = 0; i < remain; i++)
     {
-        c[epoch * 8 + i] = a[epoch * 8 + i] * b;
+        c[offset + i] = a[offset + i] * b;
     }
 }
 
@@ -100,9 +102,11 @@ void _mm256i_sub_to(int32_t *a, int32_t *b, int32_t *c, int64_t n)
         __m256i v = _mm256_sub_epi32(v1, v2);
         _mm256_storeu_si256((__m256i *)&c[i * 8], v);
     }
+
+    int offset = epoch * 8;
     for (int i = 0; i < remain; i++)
     {
-        c[epoch * 8 + i] = a[epoch * 8 + i] - b[epoch * 8 + i];
+        c[offset + i] = a[offset + i] - b[offset + i];
     }
 }
 
@@ -119,9 +123,11 @@ void _mm256i_sub_to_const_a(int32_t a, int32_t *b, int32_t *c, int64_t n)
         __m256i v = _mm256_sub_epi32(v1, v2);
         _mm256_storeu_si256((__m256i *)&c[i * 8], v);
     }
+
+    int offset = epoch * 8;
     for (int i = 0; i < remain; i++)
     {
-        c[epoch * 8 + i] = a - b[epoch * 8 + i];
+        c[offset + i] = a - b[offset + i];
     }
 }
 
@@ -138,8 +144,10 @@ void _mm256i_sub_to_const_b(int32_t *a, int32_t b, int32_t *c, int64_t n)
         __m256i v = _mm256_sub_epi32(v1, v2);
         _mm256_storeu_si256((__m256i *)&c[i * 8], v);
     }
+
+    int offset = epoch * 8;
     for (int i = 0; i < remain; i++)
     {
-        c[epoch * 8 + i] = a[epoch * 8 + i] - b;
+        c[offset + i] = a[offset + i] - b;
     }
 }
